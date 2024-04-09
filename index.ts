73b284a156ffbec186eeefa74c1fc6ca9e756588
Edwin Serna <72881312+pandaboi1@users.noreply.github.com>
2024-04-09 15:09:33 +0000

    Console output to hello

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import * as http from 'http';
 const server = http.createServer((_req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello there!\n');
+  res.end('Hello!\n');
 });
 
 class Car {
